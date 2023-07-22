a0068f44a88e6dff14541789a27aa91f41d221a8
Rajat Sundriyal <rajatsundriyal1@gmail.com>
2023-07-22 17:37:47 +0530

    Adding cart to Modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,8 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
-const Cart = () => {
-  const cartItem = (
+const Cart = (props) => {
+  const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
         <li>{item.name}</li>
@@ -10,17 +11,17 @@ const Cart = () => {
   );
 
   return (
-    <div>
-      {cartItem}
+    <Modal>
+      {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.69</span>
+        <span>35.62</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
